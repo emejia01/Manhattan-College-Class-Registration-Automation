6cfb2e54781aa66be67bdea03eb4e76a5b449e04
emejia01 <erikmej01@gmail.com>
2019-08-10 15:23:41 -0400

    Contains text used in program.

---
+++ Program_text.py
@@ -0,0 +1,29 @@
+title = "Manhattan College Class Registration Automation/Bot"
+
+directions = '''
+            Manhattan College Class Registration Automation/Bot:
+                For best results test beforehand.
+                Run the program about 1 min before registration time opens.
+            
+            Requirements:
+                1) Chrome Web Browser. Chrome needs to be installed on your computer for 
+            this program to run.
+                2) ChromeDriver executable file for your version of Chrome. That file can
+            be downloaded in the link below. Once downloaded, proceed to unzip the file.
+            
+            Directions: 
+                1) Enter your Manhattan College Self Service credentials (Make sure it's accurate).
+            Then enter the CRN numbers for the classes you want to register for (You can enter up to 4).
+                2) You then enter the File Path of the unzipped ChromeDriver file. You can do this by 
+            pressing and holding the 'option' key on your Mac keyboard and then right-clicking on the file.
+            Press the 'Copy "chromedriver" as Pathname' option. Paste it in the pathname entry-field in the 
+            AutoReg program.
+                3) Press the 'Run Bot' button.
+            
+            If your have more questions, click on the link below for more in-depth instructions.
+            
+            The program will begin to to sign you in and navigate to the class registration page. It'll 
+            refresh until registration time opens, and then register your entered classes.
+'''
+
+disclaimer = 'DISCLAIMER: May not work with some versions of Chrome. Test before use.'
